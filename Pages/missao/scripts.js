815a8d0e458779d9c142b51a005db05bcf7ae70a
dev-jef <jefersonantonio.dsp@gmail.com>
2026-03-22 01:17:34 -0300

    removi missao css problemas com api

--- Pages/missao/scripts.js
+++ Pages/missao/scripts.js
@@ -6,19 +6,23 @@ let blocoCodigo = document.querySelector(".bloco-codigo")
 let resultadoCodigo = document.querySelector(".resultado-codigo")
 let resultados = document.querySelector(".resultados")
 
+
+
 //resultados.style.display="none";
 
 
-const GROQ_API_KEY ="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"
 const endereco = "https://api.groq.com/openai/v1/chat/completions";
+
 
 async function gerarCodigo(){
 
     let txtValue = document.querySelector(".prompt").value
+    let chaveapi =  document.querySelector(".input-chaveAPI").value
+    
     let resposta = await fetch(endereco,{
         method:"POST",
         headers:{
-            "Authorization": `Bearer ${GROQ_API_KEY}`,
+            "Authorization": `Bearer ${chaveapi}`,
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
@@ -36,12 +40,19 @@ async function gerarCodigo(){
     })
     
     //resultados.style.display="flex";
+    try{
+        let dados = await resposta.json(resposta)
+        let resultados = dados.choices[0].message.content
 
-    let dados = await resposta.json(resposta)
-    let resultados = dados.choices[0].message.content
+        blocoCodigo.textContent = resultados
+        resultadoCodigo.srcdoc = resultados
+        
 
-    blocoCodigo.textContent = resultados
-    resultadoCodigo.srcdoc = resultados
-    console.log(dados)
+        alert(chaveapi)
+    }catch{
+        alert(endereco.message)
+        alert(chaveapi)
+    }
+    
 }
 botao.addEventListener("click",gerarCodigo)
